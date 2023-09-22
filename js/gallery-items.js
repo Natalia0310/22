73fd8d58c369874160087d3fc427032c222700cb
Natalia0310 <ndemydenko@ukr.net>
2023-09-22 04:35:09 +0300

    update

---
+++ js/gallery-items.js
@@ -0,0 +1,68 @@
+export const galleryItems = [
+  {
+    preview:
+      './img/photo1685629139.png.png',
+    original:
+      './img/photo1685629139.jpeg',
+    // description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      './img/4.png',
+    original:
+      './img/4.jpeg',
+    // description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      './img/photo1685632643.png.png',
+    original:
+      './img/photo1685632643.jpeg',
+    // description: 'Aerial Beach View',
+  },
+ 
+  
+  {
+    preview:
+      './img/3.png',
+    original:
+      './img/3.jpeg',
+    // description: 'Flower Blooms',
+  },
+  // {
+  //   preview:
+  //     './img/2.png',
+  //   original:
+  //     './img/2.jpeg',
+  //   // description: 'Container Haulage Freight',
+  // },
+  {
+    preview:
+      './img/1.png',
+    original:
+      './img/1.jpeg',
+    // description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      './img/6.png',
+    original:
+      './img/6.jpeg',
+    // description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      './img/photo1685632835.png.png',
+    original:
+      './img/photo1685632835.jpeg',
+    // description: 'Nature Landscape',
+  },
+  {
+    preview:
+      './img/photo1685629119.png.png',
+    original:
+      './img/photo1685629119.jpeg',
+    // description: 'Hokkaido Flower',
+  },
+ 
+];
